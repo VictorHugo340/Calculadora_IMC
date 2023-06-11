1c23d5fd17a1a8c0eda4d56905d79b09e536545c
victorhugos.540@hmail.com <victorhugos.540@gmail.com>
2023-06-11 18:20:11 -0300

    Criação da Classe pessoa com os atributos: nome, peso e altura. Leitura dos dados do usuário e classificação a partir do IMC calculado

--- bin/meu_app_calcula_imc.dart
+++ bin/meu_app_calcula_imc.dart
@@ -1,26 +1,57 @@
-void main() {}
+import 'dart:io';
+import 'dart:convert';
 
 class Pessoa {
-  final String nome;
-  final double peso;
-  final double altura;
+  String nome;
+  double peso;
+  double altura;
 
   Pessoa(this.nome, this.peso, this.altura);
 }
 
+void main() {
+  print('Calculadora de IMC');
+  print('------------------');
 
+  // Ler os dados do usuário
+  print('Digite o nome da pessoa:');
+  String nome = stdin.readLineSync(encoding: utf8)!;
 
-  /*print("Digite seu peso:");
-  var line = stdin.readLineSync(encoding: utf8);
-  var peso = double.parse(line ?? "0");
+  print('Digite o peso da pessoa (em kg):');
+  double peso = double.parse(stdin.readLineSync(encoding: utf8)!);
 
-  print("Digite sua altura:");
-  var line1 = stdin.readLineSync(encoding: utf8);
-  var altura = double.parse(line1 ?? "0");
+  print('Digite a altura da pessoa (em metros):');
+  double altura = double.parse(stdin.readLineSync(encoding: utf8)!);
 
-  double imc = calculaIMC(peso, altura);
-  print('Seu IMC é: $imc');
+  // Criar uma instância da classe Pessoa
+  Pessoa pessoa = Pessoa(nome, peso, altura);
+
+  // Calcular o IMC
+  double imc = pessoa.peso / (pessoa.altura * pessoa.altura);
+  String imcFormatado = imc.toStringAsFixed(1);
+
+  // Classificar o IMC
+  String classificacao = '';
+
+  if (imc < 18.5) {
+    classificacao = 'Abaixo do peso';
+  } else if (imc < 25) {
+    classificacao = 'Peso normal';
+  } else if (imc < 30) {
+    classificacao = 'Sobrepeso';
+  } else if (imc < 35) {
+    classificacao = 'Obesidade grau 1';
+  } else if (imc < 40) {
+    classificacao = 'Obesidade grau 2';
+  } else {
+    classificacao = 'Obesidade grau 3';
+  }
+
+  // Exibir o resultado
+  print('------------------');
+  print('Nome: ${pessoa.nome}');
+  print('Peso: ${pessoa.peso} kg');
+  print('Altura: ${pessoa.altura} m');
+  print('IMC: $imcFormatado');
+  print('Classificação: $classificacao');
 }
-
-double calculaIMC(double peso, double altura) {
-  return peso / (altura * altura);*/
